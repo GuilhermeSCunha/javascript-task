11b6f0dcbd767c5ecf6149c05ff4538b07d2edca
GuilhermeSCunha <guiasc.gui@gmail.com>
2020-10-07 01:50:41 -0300

    task done

---
+++ ex1.js
@@ -0,0 +1,8 @@
+//1 - Para passar no Colegio "Crias do Devs" os alunos precisam de media 8. Crie um código que tenha uma variavel com o valor da media do aluno no semestre retorne true ou false de acordo com o resultado do aluno no colegio.
+
+const studentGrade = 8
+let isApproved = studentGrade >= 8
+
+console.log ("está aprovado?", isApproved)
+
+
---
+++ ex2.js
@@ -0,0 +1,42 @@
+
+//2 - Verifique se as afirmativas estão certas, caso contrario justifique com uma prova real. Dica: Use o typeof para as provas reais
+
+// A) O tipo da constante `typeNumber` é number.
+const typeNumber = "10";
+
+// Falsa pq o número esta entre aspas o que faz dele uma string
+
+console.log ("A)")
+
+console.log (typeof typeNumber)
+
+// B) O valor da constante `y` é 149.
+const y = 147 + "2";
+
+//falsa pois o numero 2 é uma string então só vai concatenar e o resultado será uma string
+
+console.log ("B")
+
+console.log (y)
+
+console.log (typeof y)
+
+// C) O tipo da constante `x` é number.
+const x = 13 > 20;
+
+//falsa pq a constante `x` é uma comparação então o resultado será vedadeiro (true) ou falso (false) o que faz dela booleana
+
+console.log ("C)")
+
+console.log (typeof x)
+
+// D) O valor da constante `s` é "Estartando Devs".
+const s = "Estartando " + "Devs";
+
+//verdadeiro
+
+console.log ("D)")
+
+console.log (s)
+
+
